b21171a84a128908a19b5dfaa1f59592309560ad
5iveemmy <ainemmanuel87@gmail.com>
2023-11-25 21:37:36 +0100

    feat: app setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import { useQuery } from "@tanstack/react-query"
-import { testApi } from "@/services/sampleService"
-
 function App() {
-  const { data } = useQuery({ queryKey: ["users"], queryFn: () => testApi() })
   return (
     <div>
-      <div style={{ display: "flex", flexDirection: "column", gap: 3 }}>
-        {data?.map(({ name, id }: { name: string; id: number }) => (
-          <p key={id}>{name}</p>
-        ))}
-      </div>
+      <p>hello</p>
     </div>
   )
 }
